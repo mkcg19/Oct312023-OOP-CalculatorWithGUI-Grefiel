477cb586ac63a0407d806152e1d5342ed38bc119
mkcg19 <148318089+mkcg19@users.noreply.github.com>
2023-10-31 16:46:04 +0800

    Add files via upload

---
+++ calculatorWithGUI.java
@@ -0,0 +1,109 @@
+package OOPActivities;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class calculatorWithGUI extends JFrame {
+	private JTextField display;
+	private double num1, num2;
+	private String operator;
+	private boolean decimalPoint = false;
+	 
+	 public calculatorWithGUI() {
+	     setTitle("Calculator");
+	     setSize(300, 400);
+	     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	     setLayout(new BorderLayout());
+
+	     display = new JTextField();
+	     display.setEditable(false);
+	     add(display, BorderLayout.NORTH);
+
+	     JPanel buttonPanel = new JPanel(new GridLayout(5, 4));
+
+	     String[] buttonLabels = {
+	         "C", "+/-", "%", "/",
+	         "7", "8", "9", "*",
+	         "4", "5", "6", "-",
+	         "1", "2", "3", "+",
+	         "0", ".", "=", "←"
+	     };
+	     
+	     for (String label : buttonLabels) {
+	          JButton button = new JButton(label);
+	          button.addActionListener(new ButtonListener());
+	          buttonPanel.add(button);
+	     }
+
+	     add(buttonPanel, BorderLayout.CENTER);
+		 
+	}// end of calculatorWithGUI method
+	 
+	private class ButtonListener implements ActionListener {
+	     public void actionPerformed(ActionEvent e) {
+	         String command = e.getActionCommand();
+
+	         if ("0123456789".contains(command) || (command.equals(".") && !decimalPoint)) {
+	        	 if (command.equals(".")) {
+	                    decimalPoint = true;
+	                }
+	             display.setText(display.getText() + command);
+	         }else if ("+-*/".contains(command)) {
+	             operator = command;
+	             num1 = Double.parseDouble(display.getText());
+	             display.setText("");
+	             decimalPoint = false;
+	         }else if("%".equals(command)) {
+	        	 double currentNumber = Double.parseDouble(display.getText());
+	        	 currentNumber *= 0.01; 
+	             display.setText(String.valueOf(currentNumber));
+	         }else if ("=".equals(command)) {
+	             num2 = Double.parseDouble(display.getText());
+	             double result = calculate(num1, num2, operator);
+	             display.setText(String.valueOf(result));
+	             decimalPoint = false;
+	         }else if ("C".equals(command)) {
+	             display.setText("");
+	             decimalPoint = false;
+	         }else if("+/-".equals(command)) {
+	        	 double currentNumber = Double.parseDouble(display.getText());
+	        	 currentNumber *= -1;
+	             display.setText(String.valueOf(currentNumber));
+	         }else if("←".equals(command)) {
+	        	 String currentInput = display.getText();
+	        	 if(currentInput.length() > 0) {
+	        		 display.setText(currentInput.substring(0, currentInput.length() - 1));
+	        	 }
+	         }
+	     }
+	}
+	
+	private double calculate(double num1, double num2, String operator) {
+		switch(operator) {
+		case "+":
+			return num1 + num2;
+		case "-":
+			return num1 - num2;
+		case "*":
+			return num1 * num2;
+		case "/":
+			if(num2 != 0) {
+				return num1 / num2;
+			}else {
+				return 0;
+			}
+		default:
+			return 0;
+		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+	        calculatorWithGUI calculator = new calculatorWithGUI();
+	        calculator.setVisible(true);
+	    });
+	}
+
+}
